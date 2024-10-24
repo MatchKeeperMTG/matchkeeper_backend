53ec93a9d41fc9fdbf5be7a38117f1d236cf4664
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2024-10-24 11:03:55 -0400

    consistency

--- src/endpoints/events.js
+++ src/endpoints/events.js
@@ -4,55 +4,55 @@ import 'express';
  * @param {Express} app 
  */
 export function eventEndpoints(app) {
-    app.post('/event', (req, res) => {
+    app.post('/api/event', (req, res) => {
         res.send("Create Event");
     });
 
-    app.put('/event/:id', (req, res) => {
+    app.put('/api/event/:id', (req, res) => {
         res.send("Edit Event");
     });
 
-    app.delete('/event/:id', (req, res) => {
+    app.delete('/api/event/:id', (req, res) => {
         res.send("Delete Event");
     });
 
-    app.get('/event/:id', (req, res) => {
+    app.get('/api/event/:id', (req, res) => {
         res.send("Get Event Details");
     });
 
-    app.get('/event/:id/players', (req, res) => {
+    app.get('/api/event/:id/players', (req, res) => {
         res.send("Get Players");
     });
 
-    app.post('/event/:id/players', (req, res) => {
+    app.post('/api/event/:id/players', (req, res) => {
         res.send("Add Players");
     });
 
-    app.delete('/event/:id/players', (req, res) => {
+    app.delete('/api/event/:id/players', (req, res) => {
         res.send("Remove Players");
     });
 
-    app.post('/event/:id/decks', (req, res) => {
+    app.post('/api/event/:id/decks', (req, res) => {
         res.send("Add Deck to Event");
     });
 
-    app.delete('/event/:id/decks', (req, res) => {
+    app.delete('/api/event/:id/decks', (req, res) => {
         res.send("Remove Deck from Event");
     });
 
-    app.get('/event/:id/decks', (req, res) => {
+    app.get('/api/event/:id/decks', (req, res) => {
         res.send("View Decks from Event Library");
     });
 
-    app.get('/event/:id/bannedCards', (req, res) => {
+    app.get('/api/event/:id/bannedCards', (req, res) => {
         res.send("View Banned Cards");
     });
 
-    app.post('/event/:id/bannedCards', (req, res) => {
+    app.post('/api/event/:id/bannedCards', (req, res) => {
         res.send("Add Banned Card");
     });
 
-    app.delete('/event/:id/bannedCards', (req, res) => {
+    app.delete('/api/event/:id/bannedCards', (req, res) => {
         res.send("Unban Card");
     });
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { bracketEndpoints } from './endpoints/brackets.js';
 import { deckEndpoints } from './endpoints/deck.js';
 
 if (process.env.MONGO_URL === undefined) {
-    throw new Error("MONGO_URL is not defined, make sure .env exists and contains a valid MongoDB URL for the MONGO_URL key.");
+    throw new Error('MONGO_URL is not defined, make sure .env exists and contains a valid MongoDB URL for the MONGO_URL key.');
 }
 mongoose.connect(process.env.MONGO_URL);
 
@@ -20,7 +20,7 @@ bracketEndpoints(app);
 deckEndpoints(app);
 
 async function main() {
-    const host = process.env.SERVER_HOST ?? "localhost";
+    const host = process.env.SERVER_HOST ?? 'localhost';
     const port = process.env.SERVER_PORT ?? 8080;
 
     app.listen(port, () => {
