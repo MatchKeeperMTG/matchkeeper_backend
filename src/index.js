7f1df1fb5a7daef05981cf29ee277e4faf71203a
cdg5395 <cdg5395@psu.edu>
2024-10-28 13:56:55 -0400

    Schema base make decisions.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,64 @@ if (process.env.MONGO_URL === undefined) {
 }
 mongoose.connect(process.env.MONGO_URL);
 
+// Schemas: deck, card, user, event, bracket?
+
+//this makes the schema for the user
+const userProfileSchema = new mongoose.Schema({
+    userID : Number,
+    username: String,
+    firstName: String,
+    lastName: String,
+    userEmail: String,
+    password: String,
+    wins: Number,
+    losses: Number
+  });
+  //applies the schema to a model
+  const userProfileModel = mongoose.model('UserProfile', userProfileSchema);
+
+  const deckSchema = new mongoose.Schema({ 
+    deckID: Number,
+    deckName : String,
+    cardNames : String,
+    userID : Number,
+    deckWins : Number,
+    deckLosses : Number
+  });
+  const deckModel = mongoose.model('Deck', deckSchema);
+
+  const cardSchema = new mongoose.Schema({ 
+    cardName : String,
+    deckID : Number,
+    cardImage : String,
+    cardFormats : String,
+    cardData : String
+  });
+  const cardModel = mongoose.model('Card', cardSchema);
+
+  const eventSchema = new mongoose.Schema({ 
+    eventID : Number,
+    eventName : String,
+    location : String,
+    playerNum : Number,
+    maxPlayers : Number,
+    dateTime : Date,
+    ownerID : Number,
+    bracketID : Number
+  });
+  const eventModel = mongoose.model('Event', eventSchema);
+
+  const bracketSchema = new mongoose.Schema({ 
+    eventID : Number,
+    bracketStyle : String,
+    gameFormat : String,
+    playerNum : Number,
+    maxPlayers : Number,
+    userID : Number,
+    bracketID : Number
+  });
+  const bracketModel = mongoose.model('Bracket', bracketSchema);
+
 const app = express();
 
 // Set up endpoints
@@ -18,6 +76,7 @@ userEndpoints(app);
 eventEndpoints(app);
 bracketEndpoints(app);
 deckEndpoints(app);
+
 
 async function main() {
     const host = process.env.SERVER_HOST ?? 'localhost';
