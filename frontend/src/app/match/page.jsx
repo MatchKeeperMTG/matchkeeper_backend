12b723690839f189f1e4e33a1b0df722dd42ee7d
Cory Ross <dragong4mer404@gmail.com>
2024-12-01 13:25:43 -0500

    attempting to add a screen

--- frontend/src/app/match/page.jsx
+++ frontend/src/app/match/page.jsx
@@ -1,3 +1,53 @@
+'use client'
 export default function OtherPage() {
-    return (<div>Deep and wretched hater.</div>)
+    return (<div className="match-screen">
+        <div className="match-screen-child"></div>
+        <img className="match-screen-item" alt="" src="Ellipse 1.png" />
+        <div className="matches">Matches</div>
+        <div className="table-6-card">
+          <div className="table-6">Table 6</div>
+          <div className="table-6-card-child"></div>
+          <div className="table-6-card-item"></div>
+          <div className="player-1">PLAYER 1</div>
+          <div className="player-2">PLAYER 2</div>
+        </div>
+        <div className="table-5-card">
+          <div className="table-6">Table 5</div>
+          <div className="table-6-card-child"></div>
+          <div className="table-6-card-item"></div>
+          <div className="player-1">PLAYER 1</div>
+          <div className="player-2">PLAYER 2</div>
+        </div>
+        <div className="table-4-card">
+          <div className="table-6">Table 4</div>
+          <div className="table-6-card-child"></div>
+          <div className="table-6-card-item"></div>
+          <div className="player-1">PLAYER 1</div>
+          <div className="player-2">PLAYER 2</div>
+        </div>
+        <div className="table-3-card">
+          <div className="table-6">Table 3</div>
+          <div className="table-6-card-child"></div>
+          <div className="table-6-card-item"></div>
+          <div className="player-1">PLAYER 1</div>
+          <div className="player-2">PLAYER 2</div>
+        </div>
+        <div className="table-2-card">
+          <div className="table-6">Table 2</div>
+          <div className="table-6-card-child"></div>
+          <div className="table-6-card-item"></div>
+          <div className="player-1">PLAYER 1</div>
+          <div className="player-2">PLAYER 2</div>
+        </div>
+        <div className="table-1-card">
+          <div className="table-6">Table 1</div>
+          <div className="table-6-card-child"></div>
+          <div className="table-6-card-item"></div>
+          <div className="player-1">PLAYER 1</div>
+          <div className="player-2">PLAYER 2</div>
+        </div>
+        <div className="match-1">Match 1</div>
+        <div className="event-name">EVENT NAME</div>
+      </div>)
 }
+
