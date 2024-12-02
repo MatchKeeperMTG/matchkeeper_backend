a095a4381b9158adc741e49e70e3a75dbb957f69
Cory <dragong4mer404@gmail.com>
2024-12-01 22:10:23 -0500

    added the match page and register page

--- frontend/src/app/match/page.jsx
+++ frontend/src/app/match/page.jsx
@@ -1,53 +1,58 @@
 'use client'
+import styles from './mystyles.module.css'
+
+
 export default function OtherPage() {
-    return (<div className="match-screen">
-        <div className="match-screen-child"></div>
-        <img className="match-screen-item" alt="" src="Ellipse 1.png" />
-        <div className="matches">Matches</div>
-        <div className="table-6-card">
-          <div className="table-6">Table 6</div>
-          <div className="table-6-card-child"></div>
-          <div className="table-6-card-item"></div>
-          <div className="player-1">PLAYER 1</div>
-          <div className="player-2">PLAYER 2</div>
+    return (<div className={styles.matchScreen}>
+        <div className={styles.matchScreenChild}></div>
+        <div className={styles.matchScreenItem}/>
+        <div className= {styles.matches}>Matches</div>
+        <div className={styles.table6Card}>
+          <div className={styles.table6}>Table 6</div>
+          <div className={styles.tableCardChild}></div>
+          <div className={styles.tableCardItem}></div>
+          <div className={styles.player1}>PLAYER 1</div>
+          <div className={styles.player2}>PLAYER 2</div>
         </div>
-        <div className="table-5-card">
-          <div className="table-6">Table 5</div>
-          <div className="table-6-card-child"></div>
-          <div className="table-6-card-item"></div>
-          <div className="player-1">PLAYER 1</div>
-          <div className="player-2">PLAYER 2</div>
+        <div className={styles.table5Card}>
+          <div className={styles.table5}>Table 5</div>
+          <div className={styles.tableCardChild}></div>
+          <div className={styles.tableCardItem}></div>
+          <div className={styles.player1}>PLAYER 1</div>
+          <div className={styles.player2}>PLAYER 2</div>
         </div>
-        <div className="table-4-card">
-          <div className="table-6">Table 4</div>
-          <div className="table-6-card-child"></div>
-          <div className="table-6-card-item"></div>
-          <div className="player-1">PLAYER 1</div>
-          <div className="player-2">PLAYER 2</div>
+        <div className={styles.table4Card}>
+          <div className={styles.table4}>Table 4</div>
+          <div className={styles.tableCardChild}></div>
+          <div className={styles.tableCardItem}></div>
+          <div className={styles.player1}>PLAYER 1</div>
+          <div className={styles.player2}>PLAYER 2</div>
         </div>
-        <div className="table-3-card">
-          <div className="table-6">Table 3</div>
-          <div className="table-6-card-child"></div>
-          <div className="table-6-card-item"></div>
-          <div className="player-1">PLAYER 1</div>
-          <div className="player-2">PLAYER 2</div>
+        <div className={styles.table3Card}>
+          <div className={styles.table3}>Table 3</div>
+          <div className={styles.tableCardChild}></div>
+          <div className={styles.tableCardItem}></div>
+          <div className={styles.player1}>PLAYER 1</div>
+          <div className={styles.player2}>PLAYER 2</div>
         </div>
-        <div className="table-2-card">
-          <div className="table-6">Table 2</div>
-          <div className="table-6-card-child"></div>
-          <div className="table-6-card-item"></div>
-          <div className="player-1">PLAYER 1</div>
-          <div className="player-2">PLAYER 2</div>
+        <div className={styles.table2Card}>
+          <div className={styles.table2}>Table 2</div>
+          <div className={styles.tableCardChild}></div>
+          <div className={styles.tableCardItem}></div>
+          <div className={styles.player1}>PLAYER 1</div>
+          <div className={styles.player2}>PLAYER 2</div>
         </div>
-        <div className="table-1-card">
-          <div className="table-6">Table 1</div>
-          <div className="table-6-card-child"></div>
-          <div className="table-6-card-item"></div>
-          <div className="player-1">PLAYER 1</div>
-          <div className="player-2">PLAYER 2</div>
+        <div className={styles.table1Card}>
+          <div className={styles.table1}>Table 1</div>
+          <div className={styles.tableCardChild}></div>
+          <div className={styles.tableCardItem}></div>
+          <div className={styles.player1}>PLAYER 1</div>
+          <div className={styles.player2}>PLAYER 2</div>
         </div>
         <div className="match-1">Match 1</div>
         <div className="event-name">EVENT NAME</div>
       </div>)
+
+    // return(<div>Deep and wretched hater.</div>)
 }
 
